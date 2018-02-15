913d586c36b962cd64bceeb3fed7176171bf852a
valeria-ivasheva <valeria.ivasheva@outlook.com>
2018-02-15 16:51:58 +0300

    Added 1.1

---
+++ homework1/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            var inputString = Console.ReadLine();
+            int n = int.Parse(inputString);
+            Console.Write("Число Фибоначчи = ");
+            Console.WriteLine(Fib(n));
+        }
+        private static int Fib(int n) => n <= 2 ? 1 : Fib(n - 1) + Fib(n - 2);
+        
+    }
+}
