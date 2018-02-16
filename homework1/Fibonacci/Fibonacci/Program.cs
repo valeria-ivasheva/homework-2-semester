e9797b45b3f183689f810f52b05e3f028687f4cb
valeria-ivasheva <valeria.ivasheva@outlook.com>
2018-02-16 17:35:59 +0300

    Correct the mistakes

--- homework1/Fibonacci/Fibonacci/Program.cs
+++ homework1/Fibonacci/Fibonacci/Program.cs
@@ -10,9 +10,10 @@ namespace Fibonacci
             var inputString = Console.ReadLine();
             int n = int.Parse(inputString);
             Console.Write("Число Фибоначчи = ");
-            Console.WriteLine(Fib(n));
+            Console.WriteLine(FibonacciNumber(n));
         }
-        private static int Fib(int n) => n <= 2 ? 1 : Fib(n - 1) + Fib(n - 2);
+
+        private static int FibonacciNumber(int n) => n <= 2 ? 1 : FibonacciNumber(n - 1) + FibonacciNumber(n - 2);
         
     }
 }
