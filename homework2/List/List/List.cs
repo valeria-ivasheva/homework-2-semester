24412227149f043552d66e20e8bc4905ded727a1
valeria-ivasheva <valeria.ivasheva@outlook.com>
2018-02-24 18:53:19 +0300

    Added 2.2 list

---
+++ homework2/List/List/List.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace List
+{
+    class List
+    {
+        private class ListElement
+        {
+            public int value;
+            public ListElement next;
+
+            public ListElement(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        private ListElement head;
+        private ListElement tail;
+        private int size;
+
+        public bool IsEmpty() => size == 0;
+
+        public void Insert(int value)
+        {
+            var newElement = new ListElement(value);
+            if (IsEmpty())
+            {
+                head = newElement;
+                tail = head;
+                size++;
+                return;
+            }
+            tail.next = newElement;
+            tail = newElement;
+            size++;
+        }
+
+        public void PrintList()
+        {
+            var temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            Console.WriteLine("List:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(temp.value);
+                temp = temp.next;
+            }
+        }
+
+        public bool IsHaveElement(int value)
+        {
+            var temp = head;
+            for (int i = 0; i < size; i++)
+            {
+                if (temp.value == value)
+                {
+                    return true;
+                }
+                temp = temp.next;
+            }
+            return false;
+        }
+
+        public void DeleteElement(int value)
+        {
+            var temp = head;
+            if (temp.value == value)
+            {
+                head = head.next;
+                if (temp == tail)
+                {
+                    tail = head;
+                }
+                size--;
+                return;
+            }
+            if (!IsHaveElement(value))
+            {
+                Console.WriteLine("This element does not exist {0}", value);
+                return;
+            }
+            while (temp.next.value != value)
+            {
+                temp = temp.next;
+            }
+            if (temp.next == tail)
+            {
+                tail = temp;
+            }
+            temp.next = temp.next.next;
+            size--;
+        }
+    }
+}
---
+++ homework2/List/List/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+            list.Insert(12);
+            list.Insert(313);
+            list.Insert(-21);
+            list.Insert(0);
+            list.PrintList();
+            list.DeleteElement(-1);
+            list.DeleteElement(12);
+            list.DeleteElement(0);
+            list.Insert(732);
+            list.DeleteElement(-21);
+            list.PrintList();
+        }
+    }
+}
