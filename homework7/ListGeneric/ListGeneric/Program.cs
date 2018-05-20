6ba81826a564c4ee506b6b3eb998817b7d46fcd8
valeria-ivasheva <valeria.ivasheva@outlook.com>
2018-05-20 15:07:43 +0300

    .

--- homework7/ListGeneric/ListGeneric/Program.cs
+++ homework7/ListGeneric/ListGeneric/Program.cs
@@ -6,19 +6,7 @@ namespace ListGeneric
     {
         static void Main(string[] args)
         {
-            var list = new List<int>();
-            for (int i = 1; i <= 3; i++)
             {
-                list.Add(i);
-            }
-            list.Add(34);
-            list.Remove(34);
-            var arrayTemp = new int[3];
-            list.CopyTo(arrayTemp, 0);
-            for (int i = 1; i <= 3; i++)
-            {
-                Console.Write(list[i]);
-                Console.Write(" ");
             }
         }
     }
