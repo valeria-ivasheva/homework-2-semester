f4db0e5d50fb20bcf211134caf5dec44fabac3fe
valeria-ivasheva <valeria.ivasheva@outlook.com>
2018-06-11 18:45:22 +0300

    Плюс к забытым файлам

---
+++ homework7/Hm72/Hm72Tests/HashSetTests.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hm72.Tests
+{
+    [TestClass]
+    public class HashSetTests
+    {
+        private HashSet<int> set;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            set = new HashSet<int>();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            set.Add(22);
+            set.Add(22);
+            Assert.AreEqual(set.Count, 1);
+            set.Add(-22);
+            Assert.IsTrue(set.Contains(22));
+            Assert.IsTrue(set.Contains(-22));
+            Assert.AreEqual(2, set.Count);
+        }
+
+        [TestMethod()]
+        public void CountTest()
+        {
+            Assert.AreEqual(0, set.Count);
+            set = new HashSet<int> { 12, 23, 90, -12, 23, 0 };
+            Assert.AreEqual(5, set.Count);
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            set.Add(12);
+            set.Add(122);
+            set.Clear();
+            Assert.IsFalse(set.Contains(122));
+            Assert.IsFalse(set.Contains(12));
+            Assert.AreEqual(0, set.Count);
+        }
+
+        [TestMethod()]
+        public void ContainsTest()
+        {
+            set.Add(12);
+            set.Add(-2);
+            Assert.IsTrue(set.Contains(12));
+            Assert.IsTrue(set.Contains(-2));
+            Assert.IsFalse(set.Contains(2));
+            Assert.IsFalse(set.Contains(122));
+        }
+
+        [TestMethod()]
+        public void CopyToTest()
+        {
+            set = new HashSet<int> { 1, 2, 3 };
+            var arrayTemp = new int[3];
+            set.CopyTo(arrayTemp, 0);
+            Assert.AreEqual(arrayTemp[0], 1);
+            Assert.AreEqual(arrayTemp[1], 2);
+            Assert.AreEqual(arrayTemp[2], 3);
+        }
+
+        [TestMethod()]
+        public void ExceptWithTest()
+        {
+            var arrayTemp = new int[3] { 3, 4, 5 };
+            set = new HashSet<int> { 3, 4, 5, 56, 67, -2 };
+            set.ExceptWith(arrayTemp);
+            Assert.AreEqual(set.Count, 3);
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.IsFalse(set.Contains(arrayTemp[i]));
+            }
+        }
+
+        [TestMethod()]
+        public void IntersectWithTest()
+        {
+            var arrayTemp = new int[4] { 3, 4, 5, 3 };
+            set = new HashSet<int> { 0, 1, 2, 3, 4, 5 };
+            set.IntersectWith(arrayTemp);
+            Assert.AreEqual(3, set.Count);
+            for (int i = 3; i < 6; i++)
+            {
+                Assert.IsTrue(set.Contains(i));
+            }
+        }
+
+        [TestMethod()]
+        public void IsProperSubsetOfTest()
+        {
+            Assert.IsTrue(set.IsProperSubsetOf(set));
+            var arrayTemp = new int[3] { 1, 2, 3 };
+            set.Add(1);
+            Assert.IsTrue(set.IsProperSubsetOf(arrayTemp));
+            set.Add(-1);
+            Assert.IsFalse(set.IsProperSubsetOf(arrayTemp));
+            set = new HashSet<int> { 1, 2, 3 };
+            Assert.IsFalse(set.IsProperSubsetOf(arrayTemp));
+        }
+
+        [TestMethod()]
+        public void IsProperSupersetOfTest()
+        {
+            var arrayTemp = new int[3] { 1, 2, 3 };
+            Assert.IsFalse(set.IsProperSupersetOf(arrayTemp));
+            Assert.IsTrue(set.IsProperSupersetOf(set));
+            set = new HashSet<int> { 1, 2, 3 };
+            Assert.IsFalse(set.IsProperSupersetOf(arrayTemp));
+            set.Add(12);
+            Assert.IsTrue(set.IsProperSupersetOf(arrayTemp));
+            set = new HashSet<int> { 1, 2, 43, 33 };
+            Assert.IsFalse(set.IsProperSupersetOf(arrayTemp));
+        }
+
+        [TestMethod()]
+        public void IsSubsetOfTest()
+        {
+            Assert.IsTrue(set.IsSubsetOf(set));
+            var arrayTemp = new int[3] { 1, 2, 3 };
+            set.Add(1);
+            Assert.IsTrue(set.IsSubsetOf(arrayTemp));
+            set.Add(-1);
+            Assert.IsFalse(set.IsSubsetOf(arrayTemp));
+            set = new HashSet<int> { 1, 2, 3 };
+            Assert.IsTrue(set.IsSubsetOf(arrayTemp));
+        }
+
+        [TestMethod()]
+        public void IsSupersetOfTest()
+        {
+            var arrayTemp = new int[3] { 1, 2, 3 };
+            Assert.IsFalse(set.IsSupersetOf(arrayTemp));
+            Assert.IsTrue(set.IsSupersetOf(set));
+            set = new HashSet<int> { 1, 2, 3 };
+            Assert.IsTrue(set.IsSupersetOf(arrayTemp));
+            set.Add(12);
+            Assert.IsTrue(set.IsSupersetOf(arrayTemp));
+            set = new HashSet<int> { 1, 2, 43, 33 };
+            Assert.IsFalse(set.IsSupersetOf(arrayTemp));
+        }
+
+        [TestMethod()]
+        public void OverlapsTest()
+        {
+            Assert.IsFalse(set.Overlaps(set));
+            var arrayTemp = new int[2] { 9, 2 };
+            set = new HashSet<int> { 12 };
+            Assert.IsFalse(set.Overlaps(arrayTemp));
+            set.Add(2);
+            Assert.IsTrue(set.Overlaps(arrayTemp));
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            Assert.IsFalse(set.Remove(-1));
+            set = new HashSet<int> { 12, 2, 3 };
+            Assert.IsFalse(set.Remove(-1));
+            Assert.IsTrue(set.Remove(2));
+            Assert.IsFalse(set.Contains(2));
+            Assert.AreEqual(2, set.Count);
+            Assert.IsTrue(set.Remove(12));
+            Assert.IsFalse(set.Contains(12));
+            Assert.AreEqual(1, set.Count);
+        }
+
+        [TestMethod()]
+        public void SetEqualsTest()
+        {
+            Assert.IsTrue(set.SetEquals(set));
+            set = new HashSet<int> { 12, 1, 2 };
+            var arrayTemp = new int[4] { 12, 1, 2, -1 };
+            Assert.IsFalse(set.SetEquals(arrayTemp));
+            set.Add(-1);
+            Assert.IsTrue(set.SetEquals(arrayTemp));
+        }
+
+        [TestMethod()]
+        public void SymmetricExceptWithTest()
+        {
+            var arrayTemp = new int[4] { 1, 3, 2, 3 };
+            set.SymmetricExceptWith(arrayTemp);
+            Assert.AreEqual(set.Count, 3);
+            Assert.IsTrue(set.SetEquals(arrayTemp));
+            set.SymmetricExceptWith(arrayTemp);
+            Assert.AreEqual(0, set.Count);
+            set = new HashSet<int> { 123, 12, 2, 1 };
+            set.SymmetricExceptWith(arrayTemp);
+            Assert.AreEqual(set.Count, 3);
+            Assert.IsTrue(set.Contains(123));
+            Assert.IsFalse(set.Contains(1));
+            Assert.IsTrue(set.Contains(3));
+        }
+
+        [TestMethod()]
+        public void UnionWithTest()
+        {
+            var arrayTemp = new int[4] { 1, 2, 3, 2 };
+            set.UnionWith(arrayTemp);
+            Assert.AreEqual(set.Count, 3);
+            set.Add(45);
+            arrayTemp[3] = 90;
+            set.UnionWith(arrayTemp);
+            Assert.AreEqual(set.Count, 5);
+            Assert.IsTrue(set.Contains(45));
+            Assert.IsTrue(set.Contains(1));
+            Assert.IsTrue(set.Contains(90));
+        }
+
+        [TestMethod()]
+        public void EnumeratorTest()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                set.Add(i);
+            }
+            int j = 1;
+            foreach (var temp in set)
+            {
+                Assert.AreEqual(temp, j);
+                j++;
+            }
+        }
+    }
+}
+
